5080ada5b18d539bf831e409a194726396c00402
Victor <victorprivv101@gmail.com>
2023-09-25 21:07:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningYearObject = record.find(game => game.result === "W");
+    return winningYearObject ? winningYearObject.year : undefined;
+  }
+
+  const superBowlRecords = [
+    { year: 2020, result: "L" },
+    { year: 2021, result: "W" },
+    { year: 2022, result: "L" },
+  ];
+  
+  const winningYear = superbowlWin(superBowlRecords);
+  console.log(winningYear); // Output: 2021
+    
